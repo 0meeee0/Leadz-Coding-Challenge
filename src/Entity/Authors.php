5a9a605505d83dab280698267ebd039742ce2f58
0meeee0 <mehdi74.id@gmail.com>
2024-12-19 11:41:24 +0100

    Feat: add the validators to Author inputs

--- src/Entity/Authors.php
+++ src/Entity/Authors.php
@@ -9,6 +9,7 @@ use App\Repository\AuthorsRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: AuthorsRepository::class)]
 #[ApiResource()]
@@ -24,12 +25,15 @@ class Authors
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
     private ?string $lastName = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
     private ?string $bibliography = null;
 
     /**
