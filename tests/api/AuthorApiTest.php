d7e68db4ef071bf57d970c9db970b49e3bf1fa75
0meeee0 <mehdi74.id@gmail.com>
2024-12-23 01:00:31 +0100

    Test: add a post test to /api/authors

--- tests/api/AuthorApiTest.php
+++ tests/api/AuthorApiTest.php
@@ -18,5 +18,32 @@ class AuthorApiTest extends WebTestCase
 
         $this->assertCount(4, $data['member']);
     }
-    
+    public function testAddAuthor()
+    {
+        $client = static::createClient();
+
+        $authorData = [
+            'firstName' => 'oui',
+            'lastName' => 'non',
+            'bibliography' => 'An amazing author!',
+            'books' => [],
+        ];
+
+        $client->request('POST', '/api/authors', [], [], ['CONTENT_TYPE' => 'application/ld+json'], json_encode($authorData));
+
+        $this->assertResponseIsSuccessful();
+
+        $data = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey('id', $data);
+        $this->assertEquals('oui', $data['firstName']);
+        $this->assertEquals('non', $data['lastName']);
+        $this->assertEquals('An amazing author!', $data['bibliography']);
+
+        $authorDataFromDb = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertEquals($authorData['firstName'], $authorDataFromDb['firstName']);
+        $this->assertEquals($authorData['lastName'], $authorDataFromDb['lastName']);
+        $this->assertEquals($authorData['bibliography'], $authorDataFromDb['bibliography']);
+    }
 }
